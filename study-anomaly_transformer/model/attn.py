b997b94e324673282fab8b7b6f365e6c9ea9a5ab
guccisan <yamskymtb0903@gmail.com>
2022-11-21 14:15:38 +0900

    [add + update] 無理すぎてgitクローンした

--- study-anomaly_transformer/model/attn.py
+++ study-anomaly_transformer/model/attn.py
@@ -29,30 +29,30 @@ class AnomalyAttention(nn.Module):
         self.distances = torch.zeros((window_size, window_size)).cuda()
         for i in range(window_size):
             for j in range(window_size):
-                self.distances[i][j] = abs(i-j)
+                self.distances[i][j] = abs(i - j)
 
     def forward(self, queries, keys, values, sigma, attn_mask):
         B, L, H, E = queries.shape
         _, S, _, D = values.shape
         scale = self.scale or 1. / sqrt(E)
 
-        scores = torch.einsum("blhe, bshe->bhls", queries, keys) # !! --
+        scores = torch.einsum("blhe,bshe->bhls", queries, keys)
         if self.mask_flag:
             if attn_mask is None:
                 attn_mask = TriangularCausalMask(B, L, device=queries.device)
             scores.masked_fill_(attn_mask.mask, -np.inf)
         attn = scale * scores
 
-        sigma = sigma.transpose(1, 2) # B L H ---> B H L --
+        sigma = sigma.transpose(1, 2)  # B L H ->  B H L
         window_size = attn.shape[-1]
-        sigma = torch.sigmoid(sigma + 5) + 1e-5
+        sigma = torch.sigmoid(sigma * 5) + 1e-5
         sigma = torch.pow(3, sigma) - 1
-        sigma = sigma.unsqueeze(-1).repeat(1, 1, 1, window_size) # B H L L 
+        sigma = sigma.unsqueeze(-1).repeat(1, 1, 1, window_size)  # B H L L
         prior = self.distances.unsqueeze(0).unsqueeze(0).repeat(sigma.shape[0], sigma.shape[1], 1, 1).cuda()
         prior = 1.0 / (math.sqrt(2 * math.pi) * sigma) * torch.exp(-prior ** 2 / 2 / (sigma ** 2))
 
         series = self.dropout(torch.softmax(attn, dim=-1))
-        V = torch.einsum("bhls, bshd->blhd", series, values)
+        V = torch.einsum("bhls,bshd->blhd", series, values)
 
         if self.output_attention:
             return (V.contiguous(), series, prior, sigma)
@@ -61,18 +61,24 @@ class AnomalyAttention(nn.Module):
 
 
 class AttentionLayer(nn.Module):
-    def __init__(self, attention, d_model, n_heads, d_keys=None, d_values=None):
+    def __init__(self, attention, d_model, n_heads, d_keys=None,
+                 d_values=None):
         super(AttentionLayer, self).__init__()
 
         d_keys = d_keys or (d_model // n_heads)
         d_values = d_values or (d_model // n_heads)
         self.norm = nn.LayerNorm(d_model)
         self.inner_attention = attention
-        self.query_projection = nn.Linear(d_model, d_keys*n_heads)
-        self.key_projection = nn.Linear(d_model, d_keys*n_heads)
-        self.value_projection = nn.Linear(d_model, d_values*n_heads)
-        self.sigma_projection = nn.Linear(d_model, n_heads)
-        self.out_projection = nn.Linear(d_values*n_heads, d_model)
+        self.query_projection = nn.Linear(d_model,
+                                          d_keys * n_heads)
+        self.key_projection = nn.Linear(d_model,
+                                        d_keys * n_heads)
+        self.value_projection = nn.Linear(d_model,
+                                          d_values * n_heads)
+        self.sigma_projection = nn.Linear(d_model,
+                                          n_heads)
+        self.out_projection = nn.Linear(d_values * n_heads, d_model)
+
         self.n_heads = n_heads
 
     def forward(self, queries, keys, values, attn_mask):
@@ -94,4 +100,4 @@ class AttentionLayer(nn.Module):
         )
         out = out.view(B, L, -1)
 
-        return self.out_projection(), series, prior, sigma
+        return self.out_projection(out), series, prior, sigma
--- study-anomaly_transformer/model/embed.py
+++ study-anomaly_transformer/model/embed.py
@@ -8,11 +8,9 @@ import math
 class PositionalEmbedding(nn.Module):
     def __init__(self, d_model, max_len=5000):
         super(PositionalEmbedding, self).__init__()
-
-        # Compute the positional encodings once in log space --
+        # Compute the positional encodings once in log space.
         pe = torch.zeros(max_len, d_model).float()
-        pe.requires_grad = False 
-        # !! pe.require_grad = Falseだった。これtorchのバージョン問題があるな……
+        pe.require_grad = False
 
         position = torch.arange(0, max_len).float().unsqueeze(1)
         div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
@@ -21,7 +19,7 @@ class PositionalEmbedding(nn.Module):
         pe[:, 1::2] = torch.cos(position * div_term)
 
         pe = pe.unsqueeze(0)
-        self.register_buffer("pe", pe)
+        self.register_buffer('pe', pe)
 
     def forward(self, x):
         return self.pe[:, :x.size(1)]
@@ -30,20 +28,13 @@ class PositionalEmbedding(nn.Module):
 class TokenEmbedding(nn.Module):
     def __init__(self, c_in, d_model):
         super(TokenEmbedding, self).__init__()
-        padding = 1 if torch.__version__ >= "1.5.0" else 2
-        self.tokenConv = nn.Conv1d(
-            in_channels=c_in,
-            out_channels=d_model,
-            kernel_size=3,
-            padding=padding,
-            padding_mode="circular",
-            bias=False,
-            )
-        
+        padding = 1 if torch.__version__ >= '1.5.0' else 2
+        self.tokenConv = nn.Conv1d(in_channels=c_in, out_channels=d_model,
+                                   kernel_size=3, padding=padding, padding_mode='circular', bias=False)
         for m in self.modules():
             if isinstance(m, nn.Conv1d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_in", nonlinearity="leaky_relu")
-        
+                nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
+
     def forward(self, x):
         x = self.tokenConv(x.permute(0, 2, 1)).transpose(1, 2)
         return x
@@ -55,7 +46,7 @@ class DataEmbedding(nn.Module):
 
         self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
         self.position_embedding = PositionalEmbedding(d_model=d_model)
-    
+
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
